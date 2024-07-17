6273d45518b6fb8710591f2f9d65a673312baa98
vini carpes <vinicdf000@gmail.com>
2024-07-17 07:51:16 -0300

    first commit

---
+++ src/functionalInterface/examples/ConsumerExample.java
@@ -0,0 +1,25 @@
+package functionalInterface.examples;
+
+import java.util.Arrays;
+import java.util.List;
+// import java.util.function.Consumer;
+
+public class ConsumerExample {
+    public static void main(String[] args) {
+        List<Integer> numeros = Arrays.asList(1,2,3,4,5,6,8);
+
+        // Consumer<Integer> imprimirNumeroPar = numero ->{
+        
+        //     if(numero%2 ==0){
+        //         System.out.println(numero);
+        //     }
+        // };
+
+        numeros.stream().forEach(n ->{
+                if(n % 2 == 0){
+                    System.out.println(n);
+            }
+        }
+    );
+    }
+}
